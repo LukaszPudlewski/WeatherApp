fd7a71278f576a10d796b97817de7809517c9547
LukaszPudlewski <98075284+LukaszPudlewski@users.noreply.github.com>
2022-10-20 09:57:04 +0100

    a
    a

---
+++ src/api.js
@@ -0,0 +1,13 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '694fb26998msh94f8a014010b097p179265jsn0492c5957830',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo'
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "527a25be90efba24541cd7ca1ac87e7e"
+
